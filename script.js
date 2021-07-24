51fcdd535c024cf9ebd397ad6d0aa61d925d32d0
Murtaza Lodgher <malodgher@gmail.com>
2021-07-24 16:20:30 -0500

    Added CSV data loading

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-d3.select("h1")
-	.text("This text was updated using D3")
+async function init() {
+	const data = await d3.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv")
+	
+	d3.select("h1").text(data[0].date)
+}
