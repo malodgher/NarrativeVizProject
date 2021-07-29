8040476ca3fa8b433da7458445816d2510bccd55
Murtaza Lodgher <malodgher@gmail.com>
2021-07-29 18:58:47 -0500

    Altered code such that state data is in separate svg canvas

--- script.js
+++ script.js
@@ -8,47 +8,93 @@ async function init() {
 	});
 	
 	const states_all = data_states.map(d => d.state).filter((d, i, arr) => arr.indexOf(d) === i).sort();
-	states_all.unshift("United States")
 	
-	const dropdown = document.getElementById("statesList")
-	states_all.map(d => {
-		let opt = document.createElement('option');
-		opt.value = d;
-		opt.innerHTML = d;
-		dropdown.appendChild(opt);
-	})
+	const state_dropdown = document.getElementById("statesList")
+	const map_dropdown = document.getElementById("mapList")
+	initDropdown(state_dropdown, states_all)
+	initDropdown(map_dropdown, states_all)
 	
-	/*
-		For some reason, when using d3.max or d3.extent to get the number of cases, I don't get the
-		correct number of maximum cases, instead I get 9957805. Need to ask about that
-	*/
+	d3.select("#usCanvas").append("h2").text("Number of COVID-19 Cases in the US, 1/21/2020 - Present")
 	
-	const line_svg = d3.select("#canvas").append("svg")
-					.attr("width", (1300 + 2*(70)))
+	const us_line_svg = d3.select("#usCanvas").append("svg")
+					.attr("width", (650 + 2*(70)))
 					.attr("height", (500 + 2*(70)));
 	
-	const change_svg = d3.select("#canvas").append("svg")
-					.attr("width", (1300 + 2*(70)))
+	d3.select("#usCanvas").append("h2").text("Rate of Increase of COVID-19 cases per day in US, 1/21/2020 - Present")
+	
+	const us_change_svg = d3.select("#usCanvas").append("svg")
+					.attr("width", (650 + 2*(70)))
+					.attr("height", (500 + 2*(70)));
+	
+	const state_line_svg = d3.select("#stateCanvas").append("svg")
+					.attr("width", (650 + 2*(70)))
+					.attr("height", (500 + 2*(70)));
+	
+	const state_change_svg = d3.select("#stateCanvas").append("svg")
+					.attr("width", (650 + 2*(70)))
 					.attr("height", (500 + 2*(70)));
 	
 	const date_format = d3.timeFormat("%m/%d/%Y")
 	
-	loadUSSVG(line_svg, change_svg, data_us, date_format)
+	loadUSSVG(us_line_svg, us_change_svg, data_us, date_format)
+	loadStateSVG(state_line_svg, state_change_svg, data_states, state_dropdown.value, date_format)
 	
-	dropdown.addEventListener("change", event => {
-		if(event.target.value === "United States" || dropdown.value === "United States") {
-			line_svg.selectAll("*").remove();
-			change_svg.selectAll("*").remove();
-			loadUSSVG(line_svg, change_svg, data_us, date_format);
-		} else {
-			line_svg.selectAll("*").remove();
-			change_svg.selectAll("*").remove();
-			loadStateSVG(line_svg, change_svg, data_states, event.target.value, date_format)
-		}
+	state_dropdown.addEventListener("change", event => {
+		state_line_svg.selectAll("*").remove();
+		state_change_svg.selectAll("*").remove();
+		loadStateSVG(state_line_svg, state_change_svg, data_states, event.target.value, date_format)
 	})
 }
 
-function loadUSSVG(line_svg, change_svg, data_us, date_format) {
+function lineSetup(line_svg, data, xs, ys, date_format) {
+	line_svg.append("g").attr("transform", "translate(70,70)")
+		.append("path")
+			.datum(data)
+			.attr("class", "lineSet")
+			.attr("id", "totalCases")
+			.attr("d", d3.line()
+				.x(d => xs(d.date))
+				.y(d => ys(d.cases)));
+	
+	line_svg.append("g")
+		.attr("transform", "translate(70,570)") //translate(margin,height + margin)
+		.call(d3.axisBottom(xs)
+			.ticks(8)
+			.tickFormat(date_format));
+	
+	line_svg.append("g").attr("transform", "translate(70,70)").call(d3.axisLeft(ys));
+}
+
+function changeSetup(change_svg, data, xs, cs, date_format) {
+	change_svg.append("g").attr("transform", "translate(70,70)")
+		.attr("class", "allBars")
+		.attr("id", "caseRect")
+		.selectAll("rect")
+		.data(data).enter().append("rect")
+			.attr("width", 2)
+			.attr("height", function(d) { return (500 - cs(d.cases)); })
+			.attr("x", d => xs(d.date))
+			.attr("y", d => cs(d.cases))
+	
+	change_svg.append("g")
+		.attr("transform", "translate(70,570)") //translate(margin,height + margin)
+		.call(d3.axisBottom(xs)
+			.ticks(8)
+			.tickFormat(date_format));
+	
+	change_svg.append("g").attr("transform", "translate(70,70)").call(d3.axisLeft(cs));
+}
+
+function initDropdown(dropdown, list) {
+	list.map(d => {
+		let opt = document.createElement("option");
+		opt.value = d;
+		opt.innerHTML = d;
+		dropdown.appendChild(opt)
+	})
+}
+
+function loadUSSVG(us_line_svg, us_change_svg, data_us, date_format){
 	const change_data_us = data_us.map((d, i, data_us) => {
 		const value = {
 			date: d.date,
@@ -58,47 +104,15 @@ function loadUSSVG(line_svg, change_svg, data_us, date_format) {
 		return value;
 	});
 	
-	const xs = d3.scaleTime().domain(d3.extent(data_us, d => d.date)).range([0, 1300]);
+	const xs = d3.scaleTime().domain(d3.extent(data_us, d => d.date)).range([0, 650]);
 	const ys = d3.scaleLinear().domain([0, data_us[data_us.length - 1].cases]).range([500, 0]);
 	const cs = d3.scaleLinear().domain([/* d3.min(change_data_us, d => d.cases) */0, d3.max(change_data_us, d => d.cases)]).range([500, 0]);
 	
-	line_svg.append("g").attr("transform", "translate(70,70)")
-		.append("path")
-			.datum(data_us)
-			.attr("class", "lineSet")
-			.attr("id", "totalCases")
-			.attr("d", d3.line()
-				.x(d => xs(d.date))
-				.y(d => ys(d.cases)));
-	
-	change_svg.append("g").attr("transform", "translate(70,70)")
-		.attr("class", "allBars")
-		.attr("id", "caseRect")
-		.selectAll("rect")
-		.data(change_data_us).enter().append("rect")
-			.attr("width", 2)
-			.attr("height", function(d) { return (500 - cs(d.cases)); })
-			.attr("x", d => xs(d.date))
-			.attr("y", d => cs(d.cases))
-	
-	line_svg.append("g")
-		.attr("transform", "translate(70,570)") //translate(margin,height + margin)
-		.call(d3.axisBottom(xs)
-			.ticks(11)
-			.tickFormat(date_format));
-	
-	line_svg.append("g").attr("transform", "translate(70,70)").call(d3.axisLeft(ys));
-	
-	change_svg.append("g")
-		.attr("transform", "translate(70,570)") //translate(margin,height + margin)
-		.call(d3.axisBottom(xs)
-			.ticks(11)
-			.tickFormat(date_format));
-	
-	change_svg.append("g").attr("transform", "translate(70,70)").call(d3.axisLeft(cs));
+	lineSetup(us_line_svg, data_us, xs, ys, date_format)
+	changeSetup(us_change_svg, change_data_us, xs, cs, date_format)
 }
 
-function loadStateSVG(line_svg, change_svg, data_states, state, date_format) {
+function loadStateSVG(state_line_svg, state_change_svg, data_states, state, date_format) {
 	const data_single_state = data_states.filter((d) => d.state === state);
 	
 	const change_data_single_state = data_single_state.map((d, i, data_us) => {
@@ -111,42 +125,10 @@ function loadStateSVG(line_svg, change_svg, data_states, state, date_format) {
 		}
 		return value;
 	});
-	const xs = d3.scaleTime().domain(d3.extent(data_single_state, d => d.date)).range([0, 1300]);
+	const xs = d3.scaleTime().domain(d3.extent(data_single_state, d => d.date)).range([0, 650]);
 	const ys = d3.scaleLinear().domain([0, data_single_state[data_single_state.length - 1].cases]).range([500, 0]);
 	const cs = d3.scaleLinear().domain([0, d3.max(change_data_single_state, d => d.cases)]).range([500, 0]);
 	
-	line_svg.append("g").attr("transform", "translate(70,70)")
-		.append("path")
-			.datum(data_single_state)
-			.attr("class", "lineSet")
-			.attr("id", "totalCases")
-			.attr("d", d3.line()
-				.x(d => xs(d.date))
-				.y(d => ys(d.cases)));
-	
-	change_svg.append("g").attr("transform", "translate(70,70)")
-		.attr("class", "allBars")
-		.attr("id", "caseRect")
-		.selectAll("rect")
-		.data(change_data_single_state).enter().append("rect")
-			.attr("width", 2)
-			.attr("height", function(d) { return (500 - cs(d.cases)); })
-			.attr("x", d => xs(d.date))
-			.attr("y", d => cs(d.cases))
-	
-	line_svg.append("g")
-		.attr("transform", "translate(70,570)") //translate(margin,height + margin)
-		.call(d3.axisBottom(xs)
-			.ticks(11)
-			.tickFormat(date_format));
-	
-	line_svg.append("g").attr("transform", "translate(70,70)").call(d3.axisLeft(ys));
-	
-	change_svg.append("g")
-		.attr("transform", "translate(70,570)") //translate(margin,height + margin)
-		.call(d3.axisBottom(xs)
-			.ticks(11)
-			.tickFormat(date_format));
-	
-	change_svg.append("g").attr("transform", "translate(70,70)").call(d3.axisLeft(cs));
+	lineSetup(state_line_svg, data_single_state, xs, ys, date_format)
+	changeSetup(state_change_svg, change_data_single_state, xs, cs, date_format)
 }
